9fc8ffd898387c7bbeaa4171ff49413dc7abc334
Noam Barkai <noamb@nrgene.com>
2018-12-31 10:44:48 +0200

    Update igv.sh

--- igv.sh
+++ igv.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 CURR_SCRIPT="${0}"
-java -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:MaxGCPauseMillis=2000 -Xms4000m -Xmx30000m  -jar "${CURR_SCRIPT%/*}"/igv-nrgene-*.jar $*
+java -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -Xmx3000m -jar "${CURR_SCRIPT%/*}"/igv-nrgene-*.jar $*
